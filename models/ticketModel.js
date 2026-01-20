568a45d05681daf07507ce698fbc779faf24ddbb
amir-web-codes <amir.web.codes@gmail.com>
2026-01-20 20:51:51 +0330

    feat(ticket-model): add ticket model

---
+++ models/ticketModel.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose")
+
+const ticketSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 150
+    },
+    message: {
+        type: String,
+        required: true,
+        maxlength: 1000
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    responsedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: null
+    },
+    for: {
+        type: String,
+        enum: ["admin", "teacher"],
+        default: "admin"
+    },
+    status: {
+        type: String,
+        enum: ["open", "pending", "closed"],
+        default: "open"
+    },
+    replies: [
+        {
+            senderId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            },
+            message: String,
+            createdAt: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
+}, { timestamps: true })
+
+const ticketModel = mongoose.model("Ticket", ticketSchema)
+
+module.exports = ticketModel
