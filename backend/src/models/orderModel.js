4f1b38c69ed62919b08314f8e0d3544c05c5e877
amir-web-codes <amir.web.codes@gmail.com>
2026-07-04 23:46:38 +0330

    feat(model): add order model

---
+++ backend/src/models/orderModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+const { string } = require("zod")
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    items: [
+        {
+            courseId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+                required: true
+            },
+            title: {
+                type: String,
+            },
+            price: {
+                type: Number,
+                min: 0
+            }
+        }
+    ],
+    totalPrice: {
+        type: Number,
+        min: 0,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["pending", "paid", "failed"],
+        default: "pending"
+    }
+}, { timestamps: true })
+
+const orderModel = mongoose.model("Order", orderSchema)
+
+module.exports = orderModel
