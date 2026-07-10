44bfd93e10f929e42219afc723676e7e092d134f
amir-web-codes <amir.web.codes@gmail.com>
2026-07-10 20:53:29 +0330

    feat(documentation): add user not found to user responses

--- backend/src/docs/responses/user.response.js
+++ backend/src/docs/responses/user.response.js
@@ -1,2 +1,16 @@
 module.exports = {
+    UserNotFound: {
+        description: "Not found",
+        content: {
+            "application/json": {
+                schema: {
+                    $ref: "#/components/schemas/Error"
+                },
+                example: {
+                    success: false,
+                    message: "user not found"
+                }
+            }
+        }
+    }
 }
