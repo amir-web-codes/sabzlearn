51596b70a25e6c13748c73491ae2c7ebf5963799
amir-web-codes <amir.web.codes@gmail.com>
2026-07-12 16:47:46 +0330

    feat(documentation): add id parameter to documentation

---
+++ backend/src/docs/parameters/id.parameter.js
@@ -0,0 +1,11 @@
+module.exports = {
+    IdParameter: {
+        name: "id",
+        in: "path",
+        required: true,
+        schema: {
+            type: "string"
+        },
+        example: "6857e4d1e5d82d0d1f5d8c40"
+    }
+}
