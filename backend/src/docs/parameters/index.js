56005660d1220f4f2293299984f3326e7711c267
amir-web-codes <amir.web.codes@gmail.com>
2026-07-12 16:39:06 +0330

    feat(documentation): add index.js for documentation parameters

---
+++ backend/src/docs/parameters/index.js
@@ -0,0 +1,9 @@
+const idParameter = require("./id.parameter")
+const pageParameter = require("./page.parameter")
+const limitParameter = require("./limit.parameter")
+
+module.exports = {
+    ...idParameter,
+    ...pageParameter,
+    limitParameter
+}
