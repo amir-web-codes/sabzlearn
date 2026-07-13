31925521f0d6d104d2238b9952f3151f6d0d1980
amir-web-codes <amir.web.codes@gmail.com>
2026-07-13 10:35:30 +0330

    feat(documentation): add comment responses to API documentation

--- backend/src/docs/responses/index.js
+++ backend/src/docs/responses/index.js
@@ -2,10 +2,12 @@ const commonResponses = require("./common.response")
 const userResponses = require("./user.response")
 const ticketResponses = require("./ticket.response")
 const lessonResponses = require("./lesson.response")
+const commentResponses = require("./common.response")
 
 module.exports = {
     ...commonResponses,
     ...userResponses,
     ...ticketResponses,
-    ...lessonResponses
+    ...lessonResponses,
+    ...commentResponses
 }
