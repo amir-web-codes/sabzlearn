82a268689eaf9ae3b8ff01d59b292675597f489d
amir-web-codes <amir.web.codes@gmail.com>
2026-07-17 09:04:07 +0330

    feat(upload): add index.js for upload files

---
+++ backend/src/middlewares/uploads/index.js
@@ -0,0 +1,9 @@
+const avatarUpload = require("./avatarUpload")
+const thumbnailUpload = require("./thumbnailUpload")
+const videoUpload = require("./videoUpload")
+
+module.exports = {
+    avatarUpload,
+    thumbnailUpload,
+    videoUpload
+}
