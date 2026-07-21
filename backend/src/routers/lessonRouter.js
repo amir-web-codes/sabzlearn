a4989d4efebbd6c2c85268c9c7f5defcd0c80e74
amir-web-codes <amir.web.codes@gmail.com>
2026-07-21 09:38:02 +0330

    feat(upload): add video upload feature to lesson router

--- backend/src/routers/lessonRouter.js
+++ backend/src/routers/lessonRouter.js
@@ -7,16 +7,35 @@ const lessonController = require("../controllers/lessonController")
 
 const validator = require("../middlewares/validator")
 const lessonValidations = require("../middlewares/validations/lesson.validation")
+const { videoUpload } = require("../middlewares/uploads")
 
 router.get("/admin/get-all", validator(lessonValidations.getAllLessonsQuerySchema, "query"), checkToken, checkUserBan, checkRoles(["admin"]), limiters.adminLimiter, lessonController.getAllLessons)
 
 router.get("/:slug/get-lessons", validator(lessonValidations.getCourseLessonsQuerySchema, "query"), checkToken, checkUserBan, lessonController.getCourseLessons)
 
-router.post("/courses/:slug/create", validator(lessonValidations.createSchema), checkToken, checkUserBan, checkRoles(["admin", "teacher"]), checkSelfs.checkSelfCourseAuthor(false), lessonController.createNewLesson)
+router.post(
+    "/courses/:slug/create",
+    checkToken,
+    checkUserBan,
+    checkRoles(["admin", "teacher"]),
+    checkSelfs.checkSelfCourseAuthor(false),
+    videoUpload.single("video"),
+    validator(lessonValidations.createSchema),
+    lessonController.createNewLesson
+)
 
 router.route("/:id")
     .get(validateId, checkToken, checkUserBan, lessonController.getLessonById)
-    .patch(validateId, validator(lessonValidations.editSchema), checkToken, checkUserBan, checkRoles(["admin", "teacher"]), checkSelfs.checkSelfLessonAuthor(false), lessonController.editLesson)
+    .patch(
+        validateId,
+        checkToken,
+        checkUserBan,
+        checkRoles(["admin", "teacher"]),
+        checkSelfs.checkSelfLessonAuthor(false),
+        videoUpload.single("video"),
+        validator(lessonValidations.editSchema),
+        lessonController.editLesson
+    )
     .delete(validateId, checkToken, checkUserBan, checkRoles(["admin", "teacher"]), checkSelfs.checkSelfLessonAuthor(true), lessonController.deleteLesson)
 
 module.exports = router
